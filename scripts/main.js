237193d18710743b3c354e964ab63c88d865d974
arturparkhisenko <ikeagold@gmail.com>
2019-07-02 22:11:19 -0400

    changed deps, script iife, eslint config, removed medium

--- scripts/main.js
+++ scripts/main.js
@@ -1,143 +1,165 @@
-console.info('%c ;) Hi!', 'background: #333; color: #DCCD69');
+{
+  console.info('%c ;) Hi!', 'background: #333; color: #DCCD69');
 
-const elBody = document.querySelector('body');
+  const elBody = document.querySelector('body');
 
-// async-css-----------------------------
+  // async-css-----------------------------
 
-const cb = () => {
-  const h = document.getElementsByTagName('head')[0];
-  const l = document.createElement('link');
-  l.rel = 'stylesheet';
-  l.href = '../styles/main.min.css';
-  h.parentNode.insertBefore(l, h);
-};
-
-const raf = requestAnimationFrame || window.mozRequestAnimationFrame
-  || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
-
-if (raf !== undefined) {
-  raf(cb);
-} else {
-  window.addEventListener('load', cb);
-}
-
-// remove polyfill mdn ------------------
-if (!('remove' in Element.prototype)) {
-  Element.prototype.remove = function remove() {
-    if (this.parentNode !== undefined && this.parentNode !== null) {
-      this.parentNode.removeChild(this);
-    }
+  const cb = () => {
+    const h = document.getElementsByTagName('head')[0];
+    const l = document.createElement('link');
+    l.rel = 'stylesheet';
+    l.href = '../styles/main.min.css';
+    h.parentNode.insertBefore(l, h);
   };
-}
 
-/**
- * addEventListenerOnce
- * @param {object} target
- * @param {string} type
- * @param {function} listener
- */
-function addEventListenerOnce(target, type, listener) {
-  target.addEventListener(type, function fn(event) {
-    target.removeEventListener(type, fn);
-    listener(event);
-  }, {once: true});
-}
+  const raf =
+    requestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.msRequestAnimationFrame;
 
-// background ---------------------------
-
-const cw = window.innerWidth ||
-  document.documentElement.clientWidth ||
-  document.body.clientWidth || screen.width;
-const ch = window.innerHeight ||
-  document.documentElement.clientHeight ||
-  document.body.clientHeight || screen.height;
-let elBgImg = null;
-
-const initBg = () => {
-  window.addEventListener('online', setBg);
-  // window.addEventListener('offline', setBg);
-  setBg();
-}
-
-const setBg = () => {
-  console.info('setBg');
-
-  if (navigator.onLine === false) {
-    console.warn('Sorry, you\'re offline');
-    return;
+  if (raf !== undefined) {
+    raf(cb);
+  } else {
+    window.addEventListener('load', cb);
   }
 
-  const newCw = cw + Math.round(Math.random() * 10);
-  const newCh = ch + Math.round(Math.random() * 10);
-  const elPreScreen = document.querySelector('.screen');
-  const elPre = document.querySelector('.background');
-
-  if (elPre !== null) {
-    elPre.style.opacity = '1';
-  }
-
-  elBgImg = document.createElement('img');
-  elBgImg.classList.add('background');
-  elBgImg.setAttribute('alt', 'background');
-  elBgImg.addEventListener('load', () => {
-    document.body.insertBefore(elBgImg, elPre || elPreScreen);
-    if (elPre !== null) {
-      elPre.style.opacity = '0';
-      setTimeout(() => {
-        elPre.remove();
-      }, 400);
-    }
-  }, false);
-  elBgImg.src = `https://source.unsplash.com/${newCw}x${newCh}`;
-}
-
-// init bg ------------------------------
-
-addEventListenerOnce(elBody, 'mouseover', () => {
-  console.log('Background is loading by mouseover');
-  initBg(); // load background only on mouse move
-});
-
-addEventListenerOnce(elBody, 'touchstart', () => {
-  console.log('Background is loading by touchstart');
-  initBg(); // load background only on touch
-});
-
-// clicks--------------------------------
-
-elBody.addEventListener('click', () => {
-  setBg();
-}, false);
-
-// offline-support-----------------------
-
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('./../sw.js', {scope: '/'})
-    .then((registration) => {
-      let isUpdate = false;
-      if (registration.active) {
-        isUpdate = true;
+  // remove polyfill mdn ------------------
+  if (!('remove' in Element.prototype)) {
+    Element.prototype.remove = function remove() {
+      if (this.parentNode !== undefined && this.parentNode !== null) {
+        this.parentNode.removeChild(this);
       }
-      registration.onupdatefound = (eventUpdate) => {
-        console.log('New site update avaliable ;)');
-        // not `=>` because it doesn't create scope
-        registration.installing.onstatechange = function(eventStateChange) {
-          if (this.state === 'installed') {
-            console.log('serviceWorker installed!');
-            if (isUpdate) {
-              console.log('serviceWorker was updated, please restart tab');
+    };
+  }
+
+  /**
+   * addEventListenerOnce
+   * @param {object} target
+   * @param {string} type
+   * @param {function} listener
+   */
+  function addEventListenerOnce(target, type, listener) {
+    target.addEventListener(
+      type,
+      function fn(event) {
+        target.removeEventListener(type, fn);
+        listener(event);
+      },
+      { once: true }
+    );
+  }
+
+  // background ---------------------------
+
+  const cw =
+    window.innerWidth ||
+    document.documentElement.clientWidth ||
+    document.body.clientWidth ||
+    screen.width;
+  const ch =
+    window.innerHeight ||
+    document.documentElement.clientHeight ||
+    document.body.clientHeight ||
+    screen.height;
+  let elBgImg = null;
+
+  const initBg = () => {
+    window.addEventListener('online', setBg);
+    // window.addEventListener('offline', setBg);
+    setBg();
+  };
+
+  const setBg = () => {
+    console.info('setBg');
+
+    if (navigator.onLine === false) {
+      console.warn("Sorry, you're offline");
+      return;
+    }
+
+    const newCw = cw + Math.round(Math.random() * 10);
+    const newCh = ch + Math.round(Math.random() * 10);
+    const elPreScreen = document.querySelector('.screen');
+    const elPre = document.querySelector('.background');
+
+    if (elPre !== null) {
+      elPre.style.opacity = '1';
+    }
+
+    elBgImg = document.createElement('img');
+    elBgImg.classList.add('background');
+    elBgImg.setAttribute('alt', 'background');
+    elBgImg.addEventListener(
+      'load',
+      () => {
+        document.body.insertBefore(elBgImg, elPre || elPreScreen);
+        if (elPre !== null) {
+          elPre.style.opacity = '0';
+          setTimeout(() => {
+            elPre.remove();
+          }, 400);
+        }
+      },
+      false
+    );
+    elBgImg.src = `https://source.unsplash.com/${newCw}x${newCh}`;
+  };
+
+  // init bg ------------------------------
+
+  addEventListenerOnce(elBody, 'mouseover', () => {
+    console.log('Background is loading by mouseover');
+    initBg();
+  });
+
+  addEventListenerOnce(elBody, 'touchstart', () => {
+    console.log('Background is loading by touchstart');
+    initBg();
+  });
+
+  // clicks--------------------------------
+
+  elBody.addEventListener(
+    'click',
+    () => {
+      setBg();
+    },
+    false
+  );
+
+  // offline-support-----------------------
+
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+      .register('./../sw.js', { scope: '/' })
+      .then(registration => {
+        let isUpdate = false;
+        if (registration.active) {
+          isUpdate = true;
+        }
+        registration.onupdatefound = eventUpdate => {
+          console.log('New site update avaliable ;)');
+          // not `=>` because it doesn't create scope
+          registration.installing.onstatechange = function(eventStateChange) {
+            if (this.state === 'installed') {
+              console.log('serviceWorker installed!');
+              if (isUpdate) {
+                console.log('serviceWorker was updated, please restart tab');
+              } else {
+                console.log('App ready for offline use.');
+              }
             } else {
-              console.log('App ready for offline use.');
+              console.log('Site new serviceWorker state: ', this.state);
             }
-          } else {
-            console.log('Site new serviceWorker state: ', this.state);
-          }
+          };
         };
-      };
-    })
-    .catch((err) => {
-      console.error(err);
-    });
-} else {
-  console.log('serviceWorker is not supported');
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  } else {
+    console.log('serviceWorker is not supported');
+  }
 }
--- sw.js
+++ sw.js
@@ -1,27 +1,33 @@
-const cacheName = '20190129-1.0.0';
+const cacheName = '20190702-1.0.0';
 
-self.addEventListener('install', (event) => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(cacheName).then((cache) => cache.addAll([
-      '/',
-      '/images/dino_js.svg',
-      '/styles/main.min.css',
-      '/scripts/main.min.js',
-      '/site.webmanifest'
-    ]).then(() => self.skipWaiting()))
+    caches
+      .open(cacheName)
+      .then(cache =>
+        cache
+          .addAll([
+            '/',
+            '/images/dino_js.svg',
+            '/styles/main.min.css',
+            '/scripts/main.min.js',
+            '/site.webmanifest'
+          ])
+          .then(() => self.skipWaiting())
+      )
   );
 });
 
-self.addEventListener('activate', (event) => {
+self.addEventListener('activate', event => {
   /**
    * onActivate
    * @return {object} - promise
    */
   function onActivate() {
     // cleanup old caches
-    return caches.keys().then((savedCacheNames) => {
+    return caches.keys().then(savedCacheNames => {
       return Promise.all(
-        savedCacheNames.map((savedCacheName) => {
+        savedCacheNames.map(savedCacheName => {
           if (cacheName !== savedCacheName) {
             return caches.delete(savedCacheName);
           }
@@ -31,14 +37,14 @@ self.addEventListener('activate', (event) => {
   }
   event.waitUntil(
     onActivate()
-    // This makes the SW take effect immediately
-    // on any open pages in scope
-    .then(() => self.clients.claim())
+      // This makes the SW take effect immediately
+      // on any open pages in scope
+      .then(() => self.clients.claim())
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then((res) => res || fetch(event.request))
+    caches.match(event.request).then(res => res || fetch(event.request))
   );
 });
