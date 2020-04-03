986a413f6e710ce6baaf9687211a3b9271f9b425
arturparkhisenko <ikeagold@gmail.com>
2020-04-02 21:48:44 -0400

    change init href colors, print media styles, build system, removed some dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,46 +1,39 @@
 // LICENSE.md
 
 const path = require('path');
-
 const browserSync = require('browser-sync');
 const cssnano = require('cssnano');
 const del = require('del');
-const fancyLog = require('fancy-log');
-const PluginError = require('plugin-error');
 const postcssPresetEnv = require('postcss-preset-env');
 const postcssReporter = require('postcss-reporter');
 const TerserPlugin = require('terser-webpack-plugin');
 const webpack = require('webpack');
-
-const gulp = require('gulp');
+const { dest, parallel, series, src, watch } = require('gulp');
 const gulpLoadPlugins = require('gulp-load-plugins');
-const $ = gulpLoadPlugins();
 const gulpStylelint = require('gulp-stylelint');
 
 const production = process.env.NODE_ENV === 'production';
+const $ = gulpLoadPlugins();
+const server = browserSync.create();
+
+process.traceDeprecation = production === false;
 
 console.log(`process.env.NODE_ENV = ${process.env.NODE_ENV}`); // eslint-disable-line
 
-gulp.task('clean', () =>
-  del(['dist'], {
-    dot: true
-  })
-);
+const clean = () => del(['dist'], { dot: true });
 
-gulp.task('lintScripts', () =>
-  gulp
-    .src([
-      './scripts/**/*.js',
-      'gulpfile.js',
-      '!./scripts/**/*.min.js*',
-      '!node_modules/**'
-    ])
+const lintScripts = () =>
+  src([
+    './scripts/**/*.js',
+    'gulpfile.js',
+    '!./scripts/**/*.min.js*',
+    '!node_modules/**'
+  ])
     .pipe($.eslint())
-    .pipe($.eslint.format())
-);
+    .pipe($.eslint.format());
 
-gulp.task('lintStyles', () =>
-  gulp.src(['./styles/**/*.css', '!./styles/**/*.min.css']).pipe(
+const lintStyles = () =>
+  src(['./styles/**/*.css', '!./styles/**/*.min.css']).pipe(
     gulpStylelint({
       failAfterError: false,
       reporters: [
@@ -51,10 +44,9 @@ gulp.task('lintStyles', () =>
       ],
       debug: true
     })
-  )
-);
+  );
 
-gulp.task('scripts', cb => {
+const scripts = done => {
   webpack(
     {
       entry: './scripts/main.js',
@@ -63,7 +55,7 @@ gulp.task('scripts', cb => {
         hints: 'warning' // false, 'error'
       },
       mode: production === true ? 'production' : 'development',
-      devtool: 'source-map', // cheap-module-source-map
+      devtool: 'source-map',
       watch: false,
       output: {
         path: path.resolve(__dirname, './scripts'),
@@ -75,7 +67,10 @@ gulp.task('scripts', cb => {
             test: /\.js$/,
             exclude: /node_modules/,
             use: {
-              loader: 'babel-loader?cacheDirectory=true'
+              loader: 'babel-loader',
+              options: {
+                cacheDirectory: true
+              }
             }
           }
         ]
@@ -86,37 +81,26 @@ gulp.task('scripts', cb => {
           new TerserPlugin({
             sourceMap: production === false,
             terserOptions: {
-              output: {
-                comments: false
-              }
+              output: { comments: false }
             },
             extractComments: false
           })
         ]
       }
     },
-    (err, stats) => {
-      if (err) {
-        throw new PluginError('webpack', err);
+    (error, stats) => {
+      if (error) {
+        throw new Error(error);
       }
-      fancyLog(
-        '[webpack]',
-        stats.toString({
-          chunks: false,
-          colors: true
-        })
-      );
-      fancyLog('[webpack]', 'Packed successfully!');
+      console.log('[webpack]', stats.toString({ chunks: false, colors: true }));
+
+      done();
     }
   );
+};
 
-  cb();
-});
-
-gulp.task('styles', () =>
-  gulp
-    .src(['./styles/main.css'])
-    .pipe($.plumber())
+const styles = () =>
+  src('./styles/main.css')
     .pipe($.if(production === false, $.sourcemaps.init()))
     .pipe(
       $.postcss([
@@ -129,44 +113,24 @@ gulp.task('styles', () =>
         postcssReporter()
       ])
     )
-    .pipe(
-      $.rename({
-        suffix: '.min'
-      })
-    )
+    .pipe($.rename({ suffix: '.min' }))
     .pipe($.if(production === false, $.sourcemaps.write('./')))
-    .pipe(gulp.dest('./styles/'))
-    .pipe(
-      $.size({
-        title: 'styles'
-      })
-    )
-);
+    .pipe(dest('./styles/'))
+    .pipe($.size({ title: 'styles' }));
 
-gulp.task('images', () =>
-  gulp
-    .src(['./images/**/*'], {
-      since: gulp.lastRun('images')
-    })
+const images = () =>
+  src('./images/**/*')
     .pipe(
       $.imagemin({
         progressive: true,
         interlaced: true
       })
     )
-    .pipe(gulp.dest('./images/'))
-    .pipe(
-      $.size({
-        title: 'images'
-      })
-    )
-);
+    .pipe(dest('./images/'))
+    .pipe($.size({ title: 'images' }));
 
-gulp.task('html', () =>
-  gulp
-    .src(['./index-src.html'], {
-      since: gulp.lastRun('html')
-    })
+const html = () =>
+  src('./index-src.html')
     .pipe(
       $.htmlmin({
         collapseWhitespace: true,
@@ -174,24 +138,15 @@ gulp.task('html', () =>
       })
     )
     .pipe($.minifyInline())
-    .pipe(
-      $.rename({
-        basename: 'index'
-      })
-    )
-    .pipe(gulp.dest('./'))
-    .pipe(
-      $.size({
-        title: 'html'
-      })
-    )
-);
+    .pipe($.rename({ basename: 'index' }))
+    .pipe(dest('./'))
+    .pipe($.size({ title: 'html' }));
 
-const watch = () => {
+const reload = done => server.reload()(done);
+
+const listen = () => {
   browserSync({
     notify: false,
-    logPrefix: 'watch',
-    // https: true,
     server: './',
     files: [
       './**/*.html',
@@ -199,39 +154,44 @@ const watch = () => {
       './styles/main.css',
       './scripts/main.js'
     ],
-    port: 8080,
-    browser: 'google chrome'
+    port: 8080
   });
 
-  gulp.watch(['./**/*.html'], gulp.series('html'));
-  gulp.watch(['./styles/main.css'], gulp.series('lintStyles', 'styles'));
-  gulp.watch(['./scripts/main.js'], gulp.series('lintScripts', 'scripts'));
+  watch(['./index-src.html'], series(html, reload));
+  watch(['./styles/main.css'], series(lintStyles, styles, reload));
+  watch(['./scripts/main.js'], series(lintScripts, scripts, reload));
 };
 
-gulp.task(
-  'serve',
-  gulp.series(
-    'clean',
-    gulp.parallel(
-      gulp.series('lintScripts', 'scripts'),
-      gulp.series('lintStyles', 'styles'),
-      // 'images',
-      'html'
+const serve = () =>
+  series(
+    clean,
+    parallel(
+      series(lintScripts, scripts),
+      series(lintStyles, styles),
+      images,
+      html
     ),
-    watch
-  )
-);
+    listen
+  )();
 
-// Build production files, the default task
-gulp.task(
-  'default',
-  gulp.series(
-    'clean',
-    gulp.parallel(
-      gulp.series('lintScripts', 'scripts'),
-      gulp.series('lintStyles', 'styles'),
-      'images',
-      'html'
+const build = done =>
+  series(
+    clean,
+    parallel(
+      series(lintScripts, scripts),
+      series(lintStyles, styles),
+      images,
+      html
     )
-  )
-);
+  )(done);
+
+Object.assign(exports, {
+  build,
+  clean,
+  default: build,
+  images,
+  lintScripts,
+  lintStyles,
+  scripts,
+  serve
+});
