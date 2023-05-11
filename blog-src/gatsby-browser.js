98a04e739924c61ae6521cbd6dc85dfbeb809c77
arturparkhisenko <arturparkhisenko@users.noreply.github.com>
2023-05-10 23:25:54 -0400

    add build artifact

--- blog-src/gatsby-browser.js
+++
@@ -1,6 +0,0 @@
-// US Web Design Standards theme uses "Source Sans Pro" for body text
-import '@fontsource/source-sans-pro';
-// Logo and blog title font
-import '@fontsource/press-start-2p';
-// Highlight
-import 'prismjs/themes/prism-tomorrow.css';
