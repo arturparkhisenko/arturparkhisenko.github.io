59891daaf2e68361546f21ba54bedd91d6134997
arturparkhisenko <arturparkhisenko@users.noreply.github.com>
2024-03-06 22:15:20 -0500

    change styles, move utils

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Artur Parkhisenko - Engineer / Engineering Manager';
+export const SITE_DESCRIPTION = 'www. www always changes';
